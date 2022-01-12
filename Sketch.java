67ca514dc0bc29711968fc662140b069ae6c56cf
Ryan  Yim <75390133+RyanChiChunYim@users.noreply.github.com>
2022-01-12 16:50:08 +0000

    make snowfall and mouse drag

--- Sketch.java
+++ Sketch.java
@@ -2,13 +2,21 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
+	float[] circleY = new float[100];
+
+  int num = 100;
+  int[] x = new int[num];
+  int[] y = new int[num];
+
+
+
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(300, 300);
   }
 
   /** 
@@ -16,21 +24,52 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    for (int i = 0; i < circleY.length; i++) {
+     circleY[i] = random(height);
+    }
+
+    noStroke();
+
+    fill(255, 102);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
+    background(50);
+
+    for (int i = 0; i < circleY.length; i++) {
+     float circleX = width * i / circleY.length;
+     ellipse(circleX, circleY[i], 5, 5);
+     
+     circleY[i]++;
+
+     if (circleY[i] > height) {
+      circleY[i] = 0;
+     }
+    }
+
+    for (int i = 0; i < num-1; i++) {
+      x[i] = x[i+1];
+      y[i] = y[i+1];
+    }
+
+    x[25] = mouseX;
+    y[25] = mouseY;
+
+    for (int i = 0; i < num; i++) {
+      ellipse(x[i], y[i], i/2, i/2);
+    }
+
+
+  }
+
+  
 	  
 	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+  
   
   // define other methods down here.
 }
